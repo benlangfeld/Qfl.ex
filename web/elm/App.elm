90df3fc6a986c2c43dfb14f886845821b5dc2d26
Ben Langfeld <ben@langfeld.me>
2016-04-04 10:16:12 -0300

    docs(packaging): add docs to packages
    Allows installations to serve their own documentation relevant to the exact version installed without any external resources.

--- web/elm/App.elm
+++ web/elm/App.elm
@@ -1,6 +1,7 @@
 module App where
 
-import Html exposing (text)
+import Html exposing (..)
+import Html.Attributes exposing (..)
 
 main =
-  text "Hello, this is QueueFlex!"
+  p [] [ text "Hello, this is QueueFlex! ", a [ href "doc/index.html" ] [ text "Check out the documentation."] ]
